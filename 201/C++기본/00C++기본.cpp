95b5b174de38f939768af321cd4ae5bf10d10386
steam88ys <yssteam88@gmail.com>
2022-03-15 12:15:42 +0900

    namespace로 축약

--- 201/C++기본/00C++기본.cpp
+++ 201/C++기본/00C++기본.cpp
@@ -1,8 +1,12 @@
-#include <iostream>	//std::cout을 포함하고 있음
+// std::cout을 포함하고 있음
+#include <iostream>	
+// std::를 생략 (사용하지 않는 사람들도 많음)
+using namespace std; 
 
 int main(void)
 {
 	// std::cout객체로 문자열을 출력
-	std::cout << "안녕 세상아\n";
+	// std::endl객체로 개행처리
+	cout << "안녕 세상아"<< endl;
 	return 0;
 }
