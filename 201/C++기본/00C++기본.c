1688167d8e8d3d1806bb603e3572fda8b91fd804
steam88ys <yssteam88@gmail.com>
2022-03-15 12:07:34 +0900

    printf를 std::cout으로 대체 stdio.h를 iostream으로 대체

--- 201/C++기본/00C++기본.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("¾È³ç ¼¼»ó¾Æ\n");
-	return 0;
-}
---
+++ 201/C++기본/00C++기본.cpp
@@ -0,0 +1,8 @@
+#include <iostream>	//std::cout을 포함하고 있음
+
+int main(void)
+{
+	// std::cout객체로 문자열을 출력
+	std::cout << "안녕 세상아\n";
+	return 0;
+}
