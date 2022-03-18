4a349e5d98dd6a2312ee282139a31515fa01d2a1
steam88ys <yssteam88@gmail.com>
2022-03-18 10:29:59 +0900

    구조체 값이 바뀜(Call By Pointer)

--- 201/C기본/12 구조체 포인터.c
+++ 201/C기본/12 구조체 포인터.c
@@ -5,22 +5,22 @@ typedef struct {
 	float y;
 }Point;
 
-void AddValueToPoint(Point p);
+void AddValueToPoint(Point* p);
 
 int main(void)
 {
 	Point point;	// stack 할당
 	point.x = 5.0f;
 	point.y = 10.0f;
-	// call by value라서 값이 안바뀜
-	AddValueToPoint(point);	// 주소값을 넣어줘야 바뀜
+	// call by pointer라서 값이 바뀜
+	AddValueToPoint(&point);
 	printf("%1.f, %1.f\n", point.x, point.y);
 
 	return 0;
 }
 
-void AddValueToPoint(Point p)
+void AddValueToPoint(Point* p)
 {
-	p.x += 10.0f;
-	p.y += 15.0f;
+	p->x += 10.0f;
+	p->y += 15.0f;
 }
