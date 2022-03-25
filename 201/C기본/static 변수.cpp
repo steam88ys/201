a56c81379f037c86a73e889f3e78877a6ee3b279
steam88ys <yssteam88@gmail.com>
2022-03-25 09:22:30 +0900

    static 선언할 때

--- 201/C기본/static 변수.cpp
+++ 201/C기본/static 변수.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 void jungjuk(void)
 {
-	int a = 0;
-	cout << "지역변수 a는 " << a << endl;
+	static int a = 0;	// 한 번 할당되면 쭉 남는다.
+	cout << "정적 지역변수 a는 " << a << endl;
 	a++;
 }
 
 int main(void)
 {
 	jungjuk();	// 0
-	jungjuk();	// 0
-	jungjuk();	// 0
+	jungjuk();	// 1
+	jungjuk();	// 2
 }
