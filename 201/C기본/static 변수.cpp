7c1276b4fe32e6c9ce0d6469d79506baafa64e5f
steam88ys <yssteam88@gmail.com>
2022-03-25 09:17:47 +0900

    static을 안쓸 때

---
+++ 201/C기본/static 변수.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void jungjuk(void)
+{
+	int a = 0;
+	cout << "지역변수 a는 " << a << endl;
+	a++;
+}
+
+int main(void)
+{
+	jungjuk();	// 0
+	jungjuk();	// 0
+	jungjuk();	// 0
+}
