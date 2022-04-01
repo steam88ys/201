eec9ff411a067fe7572a921a0b0ac2e60b81849b
steam88ys <yssteam88@gmail.com>
2022-04-01 10:03:02 +0900

    매크로함수 기초

---
+++ 201/C++기본/02-01 매크로함수.cpp
@@ -0,0 +1,10 @@
+#include <iostream>	
+using namespace std; 
+
+#define jegob(A) A*A
+
+int main(void)
+{
+	cout << jegob(3) << endl;
+	cout << 3 * 3 << endl;	// 위 코드와 같음
+}
--- 201/소스.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(void)
-{
-	char str[] = "I will go to Europe";
-	for (int i = 0; str[i] != '/0'; i++)
-		cout << str[i] << endl;
-}
