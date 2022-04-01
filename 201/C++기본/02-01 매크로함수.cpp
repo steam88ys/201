219e4c762450e4dff53d8ebc7c0d472a1462c0f2
steam88ys <yssteam88@gmail.com>
2022-04-01 10:05:35 +0900

    매크로함수 오류사례

--- 201/C++기본/02-01 매크로함수.cpp
+++ 201/C++기본/02-01 매크로함수.cpp
@@ -5,6 +5,6 @@ using namespace std;
 
 int main(void)
 {
-	cout << jegob(3) << endl;
-	cout << 3 * 3 << endl;	// 위 코드와 같음
+	cout << jegob(3+1) << endl;	// 4*4가 아닌 의도치 않은 결과
+	cout << 3+1 * 3+1 << endl;	// 위 코드와 같음
 }
