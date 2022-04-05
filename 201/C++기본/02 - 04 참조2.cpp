ba2a6895a6ae90cad821d14fc182db5d226df7dd
steam88ys <yssteam88@gmail.com>
2022-04-05 11:37:30 +0900

    Revert "Call by Reference"
    This reverts commit 8da4e6a992f04014b6405da5e1a8670eed93df93.

--- 201/C++기본/02 - 04 참조2.cpp
+++ 201/C++기본/02 - 04 참조2.cpp
@@ -2,27 +2,14 @@
 using namespace std;
 
 
-//void plus2(int* num)
-//{
-//	*num += 2;	// num이 포인터 변수이기에 내부값을 *로 써서 처리
-//}
-//
-//int main(void)
-//{
-//	int a = 3;
-//	plus2(&a);	// 포인터 매개변수 num에게 주소값으로 전달
-//	cout << a << endl;
-//}
-
-// 참조형
-void plus2(int& num)
+void plus2(int* num)
 {
-	num += 2;	// num이 참조형 변수이기에 *을 안써서 내부값 처리
+	*num += 2;	// num이 포인터 변수이기에 내부값을 *로 써서 처리
 }
+
 int main(void)
 {
 	int a = 3;
-	plus2(a);	// 참조형 매개변수 num에게 전달
+	plus2(&a);	// 포인터 매개변수 num에게 주소값으로 전달
 	cout << a << endl;
-
 }
