f8166a7c76b5843b3bd19a09e6e5e3896f86c2ce
steam88ys <yssteam88@gmail.com>
2022-04-19 12:11:53 +0900

    생성자 오버로딩

--- 201/C++기본/03-01 class 생성자.cpp
+++ 201/C++기본/03-01 class 생성자.cpp
@@ -13,6 +13,7 @@ public:
 	// 생성자 : 객체가 생성되면 자동으로 호출되는 함수
 	// 반환형을 갖고있지 않는다
 	Student();
+	Student(int Hakbun, string Name);
 
 	void show();
 };
@@ -23,14 +24,26 @@ Student::Student()
 	sName = "이사랑";
 	cout << "학번이 등록되었습니다." << endl;
 }
+
+Student::Student(int Hakbun, string Name)
+{
+	nHakbun = Hakbun;
+	sName = Name;
+	cout << "학번이 등록되었습니다." << endl;
+}
+
 void Student::show()
 {
 	cout << "학번은 " << nHakbun << "입니다." << endl;
-	cout << "이름은 " << sName << "입니다." << endl;
+	cout << "이름은 " << sName << "입니다." << endl << endl;
 }
 
 int main(void)
 {
-	Student stu;
-	stu.show();
+	Student stu1 = Student();
+	stu1.show();
+
+	Student stu2 = Student(1111, "KYS");
+	stu2.show();
+
 }
