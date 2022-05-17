f0cd255f5c1704b304815dbde51fb26688adae0c
steam88ys <yssteam88@gmail.com>
2022-05-17 12:14:29 +0900

    복사생성자(얕은복사)

--- 201/C++기본/03-01 class 생성자.cpp
+++ 201/C++기본/03-01 class 생성자.cpp
@@ -14,6 +14,7 @@ public:
 	// 생성자는 반환형을 가지고 있지 않는다
 	Student();
 	Student(int Hakbun, const char* Name);
+	Student(const Student& rhs);
 	~Student();
 
 	void show();
@@ -34,6 +35,13 @@ Student::Student(int Hakbun, const char* Name)
 	strcpy(sName, Name);
 }
 
+// 직접 작성 안해도 컴파일러가 알아서 만들어주는
+// 복사생성자
+Student::Student(const Student& rhs)
+	:nHakbun(rhs.nHakbun), sName(rhs.sName)
+{
+}
+
 Student::~Student()
 {
 	delete []sName;
@@ -49,8 +57,14 @@ void Student::show()
 int main(void)
 {
 
+	// "일반생성자 호출" 출력
 	Student stu1 = Student(1111, "KYS");
+	// (1111, "KYS")가 복사됨. 일반생성자 호출x
+	Student stu2 = stu1;
+
 	stu1.show();
+	stu2.show();
+
 	return 0;
 
 }
