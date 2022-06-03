a6e82e02fea69e943828d5baf9e485c38bd7fa97
steam88ys <yssteam88@gmail.com>
2022-06-03 10:13:53 +0900

    연산자 오버로딩2

--- 201/04-01 연산자 오버로딩(기본).cpp
+++ 201/04-01 연산자 오버로딩(기본).cpp
@@ -42,7 +42,7 @@ int main(void)
 	Point p1(1, 2);
 	Point p2(3, 4); 
 
-	Point sum = p1.operator+(p2); // Point sum = p1 + p2;
+	Point sum = p1 + p2;	//Point sum = p1.operator+(p2);
 
 	sum.print();	// 4, 6
 
