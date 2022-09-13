bd86916a05161e57acf9274003c1609db12f01c5
steam88ys <yssteam88@gmail.com>
2022-09-13 12:09:54 +0900

    코드짜보기 템플릿 전

---
+++ 201/코드짜보기.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Character {
+public: 
+	string name;
+	int strength;
+	int attack;
+	int defense;
+	Character(string Cname, int Cstrength, int Cattack, int Cdefense);
+	void out();
+};
+
+Character::Character(string Cname, int Cstrength, int Cattack, int Cdefense)
+	:name(Cname), strength(Cstrength), attack(Cattack), defense(Cdefense) {
+}
+
+void Character::out() {
+	cout << "ÀÌ¸§: " << name << endl;
+	cout << "Ã¼·Â: " << strength << endl;
+	cout << "°ø°İ: " << attack << endl;
+	cout << "¹æ¾î: " << defense << endl;
+}
+
+int main(void)
+{
+	Character* ch1 = new Character("±èÀ±¼­", 5, 6, 1);
+	ch1->out();
+	delete ch1;
+}
