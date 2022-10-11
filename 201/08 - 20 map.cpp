35da6aa366c3a33b18be4072bb4f09330b61bcab
steam88ys <yssteam88@gmail.com>
2022-10-11 11:34:05 +0900

    포인터 연산 축약

--- 201/08 - 20 map.cpp
+++ 201/08 - 20 map.cpp
@@ -20,7 +20,11 @@ int main(void)
 	// vector의 모든 원소 출력(반복자)
 	map<string, string>::iterator iter;
 	for (iter = IBW.begin(); iter != IBW.end(); iter++)
-		cout << (*iter).first << "역 " << (*iter).first << "분" << endl;	// key 출력, value 출력
+		cout << (*iter).first << "역 " << (*iter).second << "분" << endl;	// key 출력, value 출력
+
+	map<string, string>::iterator iter2;
+	for (iter2 = IBW.begin(); iter2 != IBW.end(); iter2++)
+		cout << iter2->first << "역 " << iter2->second << "분" << endl;
 
 	map<string, string>::iterator main_person = IBW.find("우영우");
 	cout << "드라마의 주인공은 " << (* main_person).second;
