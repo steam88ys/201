c6632df4b4ba9f7b516f4f49dd6b1a9daf7041f8
steam88ys <yssteam88@gmail.com>
2022-10-11 11:44:07 +0900

    map을 사용하는 또 다른 방법

--- 201/08 - 20 map.cpp
+++ 201/08 - 20 map.cpp
@@ -12,10 +12,10 @@ using namespace std;
 int main(void)
 {
 	map<string, string> IBW;	// IBW: 이상한 변호사 우영우
-	IBW.insert(pair<string, string>("우영우", "박은빈"));
-	IBW.insert(pair<string, string>("정명석", "강기영"));
-	IBW.insert(pair<string, string>("권민우", "주종혁"));
-	IBW.insert(pair<string, string>("이준호", "강태오"));
+	IBW["우영우"] = "박은빈";
+	IBW["정명석"] = "강기영";
+	IBW["권민우"] = "주종혁";
+	IBW["이준호"] = "강태오";
 
 	map<string, string>::iterator iter;
 	for (iter = IBW.begin(); iter != IBW.end(); iter++)
@@ -27,5 +27,5 @@ int main(void)
 
 	// 해당 key가 존재하는지 체크
 	if (main_person != IBW.end())
-		cout << "드라마의 주인공은 " << main_person->second << endl;;
+		cout << "드라마의 주인공은 " << IBW["우영우"] << endl;;
 }
