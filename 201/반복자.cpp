b20a776d85c390c11aa5e757f7747a4b33e5f806
steam88ys <yssteam88@gmail.com>
2022-10-11 11:19:28 +0900

    map 사용하지

--- 201/반복자.cpp
+++ 201/반복자.cpp
@@ -6,9 +6,27 @@ using namespace std;
 int main(void)
 {
 	vector<int>v;
+	int input, tot = 0;
+	double avg=0;
+
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> input;
+		v.push_back(input);
+	}
+
 	v.push_back(10);
 	v.push_back(20);
 	v.push_back(30);
 	v.push_back(40);
 	v.push_back(50);
+
+	vector<int>::iterator ptr;
+	for (ptr = v.begin(); ptr != v.end(); ptr++)
+	{
+		avg += *ptr;
+		cout << *ptr << " ";
+	}
+	avg /= v.size();
+	cout << avg;
 }
